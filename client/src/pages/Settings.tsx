3ea167b6e1d2d7c6b0a122b49277b443313423e7
caio <caiorafaeldop@gmail.com>
2025-08-27 00:42:27 -0300

    organization

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -59,7 +59,7 @@ export function Settings() {
     );
   }
 
-  const isAdmin = user.userType === "admin";
+  const isAdmin = user.userType === "organization";
 
   return (
     <div className="container mx-auto py-8">
