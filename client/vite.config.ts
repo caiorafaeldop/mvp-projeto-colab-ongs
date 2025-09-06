b2ac7074853c1826684b22cec797dba82fc94935
caio <caiorafaeldop@gmail.com>
2025-09-06 18:26:02 -0300

    desfazendo ultimo commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
   },
   server: {
     host: true,
+    proxy: {
+      "/api": {
+        target: "https://mvp-colab-ongs-backend.onrender.com",
+        changeOrigin: true,
+      },
+    },
     allowedHosts: ["localhost", ".pythagora.ai", "all"],
     watch: {
       ignored: [
