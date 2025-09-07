ee0194cdb9e8094735e77fba81a2edd05ae68658
caio <caiorafaeldop@gmail.com>
2025-09-06 21:27:50 -0300

    backend

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -5,8 +5,9 @@ import axios, {
 } from "axios";
 import JSONbig from "json-bigint";
 
-// URL base do servidor backend
-const API_BASE_URL = "http://localhost:3000";
+// URL base do servidor backend 
+const API_BASE_URL = "https://mvp-colab-ongs-backend.onrender.com";
+// const API_BASE_URL = "http://localhost:3000";
 const localApi = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
