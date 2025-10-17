1dc845c518fa69d34f5ec5ef53892209f279f909
caio <caiorafaeldop@gmail.com>
2025-10-16 23:26:54 -0300

    api route na versao deploy

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -6,8 +6,8 @@ import axios, {
 import JSONbig from "json-bigint";
 
 // URL base do servidor backend 
-// const API_BASE_URL = "https://mvp-colab-ongs-backend.onrender.com";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://mvp-colab-ongs-backend.onrender.com";
+// const API_BASE_URL = "http://localhost:3000";
 const localApi = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: false,
